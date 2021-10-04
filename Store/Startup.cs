3c38f2a928f06c54a4b797c0fc17034d6b834a53
Mark Barreca <49402235+BarrecaGit@users.noreply.github.com>
2021-10-04 10:19:06 -0400

    Update Startup.cs

--- Store/Startup.cs
+++ Store/Startup.cs
@@ -52,8 +52,9 @@ namespace Store
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, ApplicationDbContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
